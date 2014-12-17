5ce862533c6ea3db2986360bcfa53ce89834b319
Will Nayes <wnayes@gmail.com>
2014-12-16 21:10:33 -0600

    Added GPR printout with value-changed indication.

--- src/debugger.c
+++ src/debugger.c
@@ -7,8 +7,28 @@
 #include <unistd.h>
 
 /*
- * Debugger vars
+ * Variables
  */
+
+// General Purpose Register names
+const char *register_names[] = {
+    "$r0",
+    "$at",
+    "v0", "v1",
+    "a0", "a1", "a2", "a3",
+    "t0", "t1", "t2", "t3", "t4", "t5", "t6", "t7",
+    "s0", "s1", "s2", "s3", "s4", "s5", "s6", "s7",
+    "t8", "t9",
+    "k0", "k1",
+    "$gp",
+    "$sp",
+    "sB",
+    "$ra"
+};
+
+// Holds the previous GPR values for comparison details.
+long long int prev_reg_values[32];
+char reg_ran_previously = 0;
 
 // Used to wait for core response before requesting next command.
 int debugger_loop_wait = 1;
@@ -28,7 +48,7 @@ void dbg_frontend_init() {
 
 void dbg_frontend_update(unsigned int pc) {
     if (!debugger_steps_pending) {
-        printf("PC at 0x%x.\n", pc);
+        printf("PC at 0x%X.\n", pc);
         debugger_loop_wait = 0;
     }
     else {
@@ -59,6 +79,27 @@ int debugger_set_run_state(int state) {
 int debugger_step() {
     m64p_error rval = (*DebugStep)();
     return rval != M64ERR_SUCCESS;
+}
+
+int debugger_print_registers() {
+    unsigned long long int *regs = (*DebugGetCPUDataPtr)(M64P_CPU_REG_REG);
+    if (regs == NULL)
+        return -1;
+
+    printf("General Purpose Registers:\n");
+    int i;
+    for (i = 0; i < 32; ++i) {
+        char val_changed = reg_ran_previously && regs[i] != prev_reg_values[i];
+        if (val_changed)
+            printf("%c[1m", 27); // Bold on
+        printf("%4s %016X ", register_names[i], regs[i]);
+        if (val_changed)
+            printf("%c[0m", 27); // Bold off
+        reg_ran_previously = 1;
+        prev_reg_values[i] = regs[i];
+        if (i % 2 != 0)
+            printf("\n");
+    }
 }
 
 /*
@@ -102,6 +143,9 @@ void *debugger_loop(void *arg) {
                 debugger_steps_pending = 1;
             --debugger_steps_pending;
             debugger_step();
+        }
+        else if (strcmp(input, "regs") == 0) {
+            debugger_print_registers();
         }
         else if (strlen(input) == 0)
             continue;
